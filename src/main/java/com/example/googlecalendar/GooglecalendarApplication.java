98fb94a120d4eb9bd9d7416fae0eb0a20d29a291
son <handsun000@gmail.com>
2021-01-28 22:10:39 +0900

    Initial commit

---
+++ src/main/java/com/example/googlecalendar/GooglecalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.googlecalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglecalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglecalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/googlecalendar/GooglecalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googlecalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglecalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
